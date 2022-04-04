cb586f2e6a17462767859e1d5c6d0e4ef6ec159d
parkerkschmidt <parkerschmidt05@gmail.com>
2022-04-04 12:52:11 -0600

    added context file, conntection strings.

---
+++ UtahMVC/Models/CrashesDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace UtahMVC.Models
+{
+    public class CrashesDbContext : DbContext
+    {
+        public CrashesDbContext(DbContextOptions<CrashesDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Crash> Crashes { get; set; }
+    }
+}
