893ba5a7b0e2b33eaf7cd57bc22b92014aa32966
parkerkschmidt <parkerschmidt05@gmail.com>
2022-04-04 12:19:14 -0600

    bootstrap. not everything works yet.

--- UtahMVC/Controllers/HomeController.cs
+++ UtahMVC/Controllers/HomeController.cs
@@ -11,11 +11,9 @@ namespace UtahMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
@@ -23,15 +21,16 @@ namespace UtahMVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Search()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+
+        public IActionResult Crashes()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
+
     }
 }
